f62a586accc4611dabfe414cef90981639fc2818
unknown <h0651302@gmail.com>
2026-04-01 11:48:30 +0530

    fix: production paths for favicon and images, optimized structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,3 +5,12 @@ extend: {},
   },
 plugins: [],
 }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
